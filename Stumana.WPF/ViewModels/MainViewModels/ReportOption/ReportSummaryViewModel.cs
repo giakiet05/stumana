1fcaa6ae7c25acd2c55097a5d6e5213ae076c282
Võ Hồng Lương <150911675+luong-vh@users.noreply.github.com>
2025-04-21 23:23:08 +0700

    Add ViewModels and map them to Views via DataTemplates

---
+++ Stumana.WPF/ViewModels/MainViewModels/ReportOption/ReportSummaryViewModel.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Windows.Input;
+
+namespace Stumana.WPF.ViewModels.MainViewModels.ReportOption
+{
+    public class ReportSummaryViewModel: BaseViewModel
+    {
+        public ICommand ReportSubjectCommand { get; }
+        public ReportSummaryViewModel()
+        {
+        }
+    }
+   
+}

05283681ae33757b917f513d2c9c3e8305abf9ca
Võ Hồng Lương <150911675+luong-vh@users.noreply.github.com>
2025-04-23 18:27:25 +0700

    Add SidebarViewModel and map with SidebarView

---
+++ Stumana.WPF/ViewModels/MainViewModels/SidebarViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Stumana.WPF.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stumana.WPF.ViewModels.MainViewModels
+{
+    public class SidebarViewModel: BaseViewModel
+    {
+        public BaseViewModel CurrentSidebarViewModel => NavigationStore.Instance.CurrentLayoutModel;
+    }
+}

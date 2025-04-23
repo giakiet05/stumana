37a1af36c740e7219a70492ebca4cebde7efcffd
Võ Hồng Lương <150911675+luong-vh@users.noreply.github.com>
2025-04-23 19:40:33 +0700

    STRANGE BUG

--- Stumana.WPF/ViewModels/MainViewModels/SidebarViewModel.cs
+++ Stumana.WPF/ViewModels/MainViewModels/SidebarViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Stumana.WPF.Stores;
 using Stumana.WPF.ViewModels.MainViewModels.AccountOption;
+using Stumana.WPF.ViewModels.MainViewModels.ClassOption;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,25 @@ namespace Stumana.WPF.ViewModels.MainViewModels
 {
     public class SidebarViewModel : BaseViewModel
     {
+        private string text;
+
+
+        public string Text
+        {
+            get { return text; }
+            set {
+                text = value; 
+                OnPropertyChanged(nameof(Text));
+            }
+        }
+
         public BaseViewModel CurrentSidebarViewModel => NavigationStore.Instance.CurrentLayoutModel;
 
         public SidebarViewModel()
         {
             NavigationStore.Instance.CurrentLayoutModelChanged += OnCurrentLayoutModelChanged;
-            NavigationStore.Instance.CurrentLayoutModel = new AccountOptionViewModel();
+            NavigationStore.Instance.CurrentLayoutModel = new ClassListViewModel();
+            Text = "Testing";
         }
         private void OnCurrentLayoutModelChanged()
         {

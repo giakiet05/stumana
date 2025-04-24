4c5cea39b7059d61c8f535e848c37c46a5c5d935
Khoi Vo <23520790@gm.uit.edu.vn>
2025-04-24 16:28:05 +0700

    Classroom ViewModel

--- Stumana.WPF/ViewModels/MainViewModels/ClassOption/ClassListViewModel.cs
+++ Stumana.WPF/ViewModels/MainViewModels/ClassOption/ClassListViewModel.cs
@@ -1,12 +1,138 @@
 ﻿
+using System.Collections.ObjectModel;
 using System.Windows.Input;
+using Stumana.DataAccess.Services;
+using Stumana.DataAcess.Models;
 
 namespace Stumana.WPF.ViewModels.MainViewModels.ClassOption
 {
     public class ClassListViewModel: BaseViewModel
     {
+        #region Commands
         public ICommand exportCommand { get; }
-        public ClassListViewModel() { }
 
+        #endregion Commands
+
+        #region Properties
+
+        private Dictionary<string, SchoolYear> SchoolYearsDic = new Dictionary<string, SchoolYear>();
+
+        private ObservableCollection<string> _schoolyearCollection;
+        public ObservableCollection<string> SchoolYearCollection
+        {
+            get => _schoolyearCollection;
+            set
+            {
+                _schoolyearCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedschoolyear;
+        public string SelectedSchoolYear
+        {
+            get => _selectedschoolyear;
+            set
+            {
+                _selectedschoolyear = value;
+                OnPropertyChanged();
+                SelectionChange();
+            }
+        }
+
+        private Dictionary<string, Grade> GradeDic = new Dictionary<string, Grade>();
+
+        private ObservableCollection<string> _gradeCollection;
+        public ObservableCollection<string> GradeCollection
+        {
+            get => _gradeCollection;
+            set
+            {
+                _gradeCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _selectedgrade;
+        public string SelectedGrade
+        {
+            get => _selectedgrade;
+            set
+            {
+                _selectedgrade = value;
+                OnPropertyChanged();
+                SelectionChange();
+            }
+        }
+
+        private ObservableCollection<Classroom> _classroomCollection;
+        public ObservableCollection<Classroom> ClassroomCollection
+        {
+            get => _classroomCollection;
+            set
+            {
+                _classroomCollection = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion Properties
+       
+
+        public ClassListViewModel()
+        {
+            LoadSchoolYear();
+            LoadGrade();
+        }
+
+        private async void SelectionChange()
+        {
+            if (string.IsNullOrEmpty(SelectedGrade) || string.IsNullOrEmpty(SelectedSchoolYear))
+            {
+                ClassroomCollection.Clear();
+                return;
+            }
+
+            IEnumerable<Classroom> classrooms;
+
+            if (SelectedSchoolYear != "Tất cả các năm")
+            {
+                SchoolYear curSchoolYear = SchoolYearsDic[SelectedSchoolYear];
+                classrooms = await GenericDataService<Classroom>.Instance.GetManyAsync(cl => cl.YearId == curSchoolYear.Id);
+            }
+            else
+                classrooms = await GenericDataService<Classroom>.Instance.GetAllAsync();
+
+            if (SelectedGrade != "Tất cả các khối")
+            {
+                Grade curGrade = GradeDic[SelectedGrade];
+                classrooms = classrooms.Where(cl => cl.GradeId == curGrade.Id);
+            }
+
+            ClassroomCollection = new ObservableCollection<Classroom>(classrooms);
+        }
+
+        private async void LoadSchoolYear()
+        {
+            var sy = await GenericDataService<SchoolYear>.Instance.GetAllAsync();
+            foreach (SchoolYear schoolYear in sy)
+            {
+                string schoolyearstr = schoolYear.StartYear + "-" + schoolYear.EndYear;
+                SchoolYearCollection.Add(schoolyearstr);
+                SchoolYearsDic.Add(schoolyearstr, schoolYear);
+            }
+            GradeCollection.Add("Tất cả các năm");
+        }
+
+        private async void LoadGrade()
+        {
+            var grades = await GenericDataService<Grade>.Instance.GetAllAsync();
+            foreach (Grade grade in grades)
+            {
+                GradeCollection.Add("Khối" + grade.Level);
+                GradeDic.Add("Khối" + grade.Level, grade);
+            }
+            GradeCollection.Add("Tất cả các khối");
+        }
     }
 }

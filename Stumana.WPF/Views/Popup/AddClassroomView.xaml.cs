059df0eb918b3bbe2f47953ac8e9c98f217f2cbb
Andoryou <nguyenan.shigoto@gmail.com>
2025-04-25 21:57:21 +0700

    AddClassroom_SemesterView

---
+++ Stumana.WPF/Views/Popup/AddClassroomView.xaml.cs
@@ -0,0 +1,16 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Stumana.WPF.Views.Popup
+{
+    /// <summary>
+    /// Interaction logic for AddClassroomView.xaml
+    /// </summary>
+    public partial class AddClassroomView : UserControl
+    {
+        public AddClassroomView()
+        {
+            InitializeComponent();
+        }
+    }
+}

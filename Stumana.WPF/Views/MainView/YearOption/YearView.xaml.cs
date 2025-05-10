9db756a2f707e034ff8879152d10ea2829fb67dc
Võ Hồng Lương <luong25105@gmail.com>
2025-05-10 21:18:02 +0700

    Delete excess file

--- Stumana.WPF/Views/MainView/YearOption/YearView.xaml.cs
+++ Stumana.WPF/Views/MainView/YearOption/YearView.xaml.cs
@@ -13,14 +13,11 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Stumana.WPF.Views.MainView.SubjectOption
+namespace Stumana.WPF.Views.MainView.YearOption
 {
-    /// <summary>
-    /// Interaction logic for AddSubjectView_2.xaml
-    /// </summary>
-    public partial class AddSubjectView_2 : UserControl
+    public partial class YearView : UserControl
     {
-        public AddSubjectView_2()
+        public YearView()
         {
             InitializeComponent();
         }

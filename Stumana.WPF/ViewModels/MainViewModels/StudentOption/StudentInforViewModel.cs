8487eea0095c7da0c0125e166aff5e24630fc9fb
Võ An Khôi <voankhoi@gmail.com>
2025-06-01 23:17:49 +0700

    data bind for Student Info ViewModel

--- Stumana.WPF/ViewModels/MainViewModels/StudentOption/StudentInforViewModel.cs
+++ Stumana.WPF/ViewModels/MainViewModels/StudentOption/StudentInforViewModel.cs
@@ -1,14 +1,63 @@
-﻿using Stumana.DataAccess.Services;
-using Stumana.DataAcess.Models;
+﻿using Stumana.DataAcess.Models;
 using Stumana.WPF.Commands;
 using Stumana.WPF.Stores;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Windows.Input;
 
 namespace Stumana.WPF.ViewModels.MainViewModels.StudentOption
 {
     public class StudentInfoViewModel : BaseViewModel
     {
+        private DataTable _scoreDataTable = new DataTable();
+
+        public DataTable ScoreDataTable
+        {
+            get => _scoreDataTable;
+            set
+            {
+                _scoreDataTable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DataView _tableView;
+
+        public DataView TableView
+        {
+            get => _tableView;
+            set
+            {
+                _tableView = value;
+                OnPropertyChanged(nameof(TableView));
+            }
+        }
+
+        private string _studentId;
+
+        public string StudentId
+        {
+            get => _studentId;
+            set
+            {
+                _studentId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _studentName;
+
+        public string StudentName
+        {
+            get => _studentName;
+            set
+            {
+                _studentName = value;
+                OnPropertyChanged();
+            }
+        }
+
+
         public Dictionary<string, SchoolYear> SchoolYearDic { get; set; } = new();
         private ObservableCollection<string> _schoolyearFilter = new();
 
@@ -61,14 +110,79 @@ namespace Stumana.WPF.ViewModels.MainViewModels.StudentOption
             }
         }
 
+        private string _classroom;
+
+        public string Classroom
+        {
+            get => _classroom;
+            set
+            {
+                _classroom = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _absence;
+
+        public string Absence
+        {
+            get => _absence;
+            set
+            {
+                _absence = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public ObservableCollection<string> GradingCollection { get; set; }
+        private string _selectedGrading;
+
+        public string SelectedGrading
+        {
+            get => _selectedGrading;
+            set
+            {
+                _selectedGrading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<string> ConductCollection { get; set; }
+        private string _selectedConduct;
+
+        public string SelectedConduct
+        {
+            get => _selectedConduct;
+            set
+            {
+                _selectedConduct = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string _evaluation;
+
+        public string Evaluation
+        {
+            get => _evaluation;
+            set
+            {
+                _evaluation = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ICommand CloseModalCommand { get; set; }
 
         public StudentInfoViewModel(Student? student)
         {
-            CloseModalCommand = new RelayCommand(ModalNavigationStore.Instance.Close);
+            CloseModalCommand = new RelayCommand(SaveInfo);
 
             LoadSchoolYearFilter();
             LoadSemesterFilter();
+
+            Classroom = "10A3";
         }
 
         private async void LoadSchoolYearFilter()
@@ -78,10 +192,24 @@ namespace Stumana.WPF.ViewModels.MainViewModels.StudentOption
 
         private async void LoadSemesterFilter()
         {
-            if (SelectedSchoolYear == null) 
-                return;
-
             SemesterFilter.Clear();
+            if (SelectedSchoolYear == null)
+                return;
+        }
+
+        private async void LoadTableColumn()
+        {
+            ScoreDataTable = new DataTable();
+            ScoreDataTable.Columns.Add("Môn học", typeof(string));
+
+
+            ScoreDataTable.Columns.Add("Điểm TB", typeof(double));
+            TableView = ScoreDataTable.DefaultView;
+        }
+
+        private void SaveInfo()
+        {
+            ModalNavigationStore.Instance.Close();
         }
     }
 }
